e864554aced08fb6da6f0f9344b34d3053c8f74e
ymym_glb <ymym_glb@163.com>
2020-07-28 20:41:03 +0800

    "添加一个test类"

---
+++ mybatis-demo1/src/main/java/com/ymym/domain/Test01.java
@@ -0,0 +1,13 @@
+package com.ymym.domain;
+
+/**
+ * @author ymyoo
+ * @date 2020-07-28 20:39
+ * @since
+ */
+public class Test01 {
+
+    public void add(){
+        System.out.println("添加一个");
+    }
+}
